ead8507164c74c3ad7a9176e4e6dc5eee4203646
olcaycanitez <olcaycanitez@gmail.com>
2020-08-09 11:35:40 -0700

    CarMax

--- Summer2020_B20/src/day39_CustomClass/Car.java
+++ Summer2020_B20/src/day39_CustomClass/Car.java
@@ -1,13 +1,11 @@
 package day39_CustomClass;
 
 public class Car {
-
     /*
-
-    Attributes:
-    model, brand, year, milage, color, price
-
+        Attributes:
+            model, brand, year, color, mileage, price
      */
+
 
     String brand;
     String model;
@@ -16,7 +14,33 @@ public class Car {
     double mileage;
     double price;
 
+    public void setCarInfo(String carBrand, String carModel, int carYear, String carColor, double carMileage, double carPrice){
+        brand = carBrand;
+        model = carModel;
+        year = carYear;
+        color = carColor;
+        mileage = carMileage;
+        price = carPrice;
+    }
+    // sets the info of the car objects
 
 
+    // 2012 Toyota Corolla, Red, 10000, $45000
+    public void getCarInfo(){
+        System.out.println(year +" "+brand+" "+model+", "+ color+", "+mileage+", $"+price);
+    }
+    // prints the car info
+
+    public void start(){
+        System.out.println(brand + " " + model + " is started");
+    }
+
+
+
+    /*
+    car1: Toyota
+    car2: BMW
+    car3: Tesla
+     */
 
 }
